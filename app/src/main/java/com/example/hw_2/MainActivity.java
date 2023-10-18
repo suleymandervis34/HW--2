242152d977932f19200437918888fb4af22757db
homeWork <suleymandervis34@gmail.com>
2023-10-19 01:09:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw_2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hw_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().add(R.id.conteyner,new FirstFragment()).commit();
+    }
+}
